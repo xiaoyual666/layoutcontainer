de47fcfca5559e89a9e637f4fe6299a2bf7fe5a2
xiaoyual <xiaoyuan.liu@mathworks.com>
2018-04-28 15:14:17 +0100

    add source link

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,7 +6,7 @@ module.exports = merge(common, {
     devtool: "source-map",
     mode: "development",
     devServer: {
-        contentBase: path.join(__dirname, "dist"),
+        contentBase: path.join(__dirname),
         compress: true,
         port: 9000
     }
