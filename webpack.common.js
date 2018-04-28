ddfd2c357abf28c12485b0cf6a64cd051d880594
xiaoyual <xiaoyuan.liu@mathworks.com>
2018-04-28 15:17:39 +0100

    add source link

--- webpack.common.js
+++ webpack.common.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: {
-        LayoutContainer: "./index.js",
+        // LayoutContainer: "./index.js",
         example: "./example/example.js"
     },
     output: {
