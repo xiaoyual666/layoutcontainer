c10c94d26f31bd5cca9444742d9292f1801ab2ab
xiaoyual <xiaoyuan.liu@mathworks.com>
2018-04-29 19:29:00 +0100

    upload images

--- webpack.common.js
+++ webpack.common.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: {
-        LayoutContainer: "./index.js"
+        example: "./index.js"
     },
     output: {
         path: path.resolve(__dirname, "dist"),
